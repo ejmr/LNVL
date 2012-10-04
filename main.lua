8f3a9b9c72c746f3954aeb3d2725422f890fbad9
Eric James Michael Ritz <lobbyjones@gmail.com>
2012-10-04 09:42:47 -0400

    Add a ‘main.lua’ file for testing LNVL with LÖVE

---
+++ main.lua
@@ -0,0 +1,23 @@
+--[[
+--
+-- The main file required by LÖVE.  We do not use this for anything in
+-- LNVL except debugging, since we ship out LNVL as a library for
+-- other programs to use and not really an executable on its own.
+--
+-- The program tests if we can create and draw an LNVL.Scene object.
+--
+--]]
+
+require("LNVL")
+
+scene = nil
+
+function love.load(arguments)
+    love.graphics.setMode(800, 600)
+    love.graphics.setBackgroundColor(0, 0, 0)
+    scene = LNVL.Scene{}
+end
+
+function love.draw()
+    scene:draw()
+end
