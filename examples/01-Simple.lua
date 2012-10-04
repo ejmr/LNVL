e963569740328882e06a1605240ad9a98bce202f
Eric James Michael Ritz <lobbyjones@gmail.com>
2012-10-04 18:15:20 -0400

    Add a missing comma on line 14 of example-01
    This also changes the example to use the LNVL.Scene:new{} constructor
    that we use now, since the old idea of just using LNVL.Scene{}
    directly is no longer part of the design.

--- examples/01-Simple.lua
+++ examples/01-Simple.lua
@@ -5,13 +5,13 @@
 -- adventure or RPG in my opinion.  My English translation of the
 -- chorus and verse parts used are in the comments next to the text.
 
-START = LNVL.Scene{
-    "休まずにKING 働くよKING",           -- Without resting, our King, Always working, our King
-    "人の庭に全て足りるまで",             -- Until this garden for humans has all it needs
-    "たんと吹け風よ ダントツに爽快に",     -- A great wind blows, refreshing all
-    "パンパンにシャツを 帆のように張らせ",  -- Fill your shirt, like the bursting sail of a ship
-    "たんと吹け風よ 人体の宇宙（ そら） に" -- A great wind blows, throughout this universe of humans
-    "働け庭師 休まずＫ Ｉ Ｎ Ｇ",         -- Keep working, our Gardener, Without resting, our King
+START = LNVL.Scene:new{
+    "休まずにKING 働くよKING",            -- Without resting, our King, Always working, our King
+    "人の庭に全て足りるまで",              -- Until this garden for humans has all it needs
+    "たんと吹け風よ ダントツに爽快に",      -- A great wind blows, refreshing all
+    "パンパンにシャツを 帆のように張らせ",   -- Fill your shirt, like the bursting sail of a ship
+    "たんと吹け風よ 人体の宇宙（ そら） に", -- A great wind blows, throughout this universe of humans
+    "働け庭師 休まずＫ Ｉ Ｎ Ｇ",          -- Keep working, our Gardener, Without resting, our King
 }
 
 -- By default scenes take up a pre-define area on screen.  So for
