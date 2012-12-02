f9cfd71682bfccbe8d371c033b3440272d3ae31c
Eric James Michael Ritz <lobbyjones@gmail.com>
2012-12-02 14:15:46 -0500

    Implement LNVL.ClampedArray()

--- src/clamped-array.lua
+++ src/clamped-array.lua
@@ -17,3 +17,48 @@
 -- So the values of 'foo' are {10, nil, 8}' now.
 --
 --]]
+
+LNVL.ClampedArray = {}
+
+-- The constructor takes one argument, which is the maximum index
+-- value the array allows (inclusive).  We store this in the
+-- 'maxmimum_index' property.
+function LNVL.ClampedArray:new(max)
+    local array = {}
+    setmetatable(array, LNVL.ClampedArray)
+    array.maximum_index = max
+    return array
+end
+
+-- The __index() metamethod ensures that we do not exceed the bounds
+-- of the array's indexes.  If 'key' is not a number then we return
+-- immediately because we do not perform the check because that means
+-- we could be looking up a property name.
+LNVL.ClampedArray.__index =
+    function (table, key)
+        if type(key) == "number" then
+            if key < 1 then
+                key = 1
+            elseif key > table.maximum_index then
+                key = table.maximum_index
+            end
+        end
+
+        return rawget(table, key)
+    end
+
+-- The __newindex() metamethod works similarly to __index(), ensuring
+-- that we cannot set numeric indexes above the maximum assigned when
+-- we constructed the array.
+LNVL.ClampedArray.__newindex =
+    function (table, key, value)
+        if type(key) == "number" then
+            if key < 1 then
+                key = 1
+            elseif key > table.maximum_index then
+                key = table.maximum_index
+            end
+        end
+
+        rawset(table, key, value)
+    end
