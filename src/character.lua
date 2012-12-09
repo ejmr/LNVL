922bfd1eb7f3280dc3a67b4926693b59a41b17d2
Eric James Michael Ritz <lobbyjones@gmail.com>
2012-12-09 17:59:42 -0500

    Fix the indentation in the LNVL.Character code
    This patch only changes whitespace.  It contains no logic changes.

--- src/character.lua
+++ src/character.lua
@@ -14,34 +14,34 @@ function LNVL.Character:new(properties)
     local character = {}
     setmetatable(character, LNVL.Character)
 
-     -- name: The name of the character as a string.  Right now we set
-     -- it as an empty string because the loop later through
-     -- 'properties' should give it a value.  If it does not then we
-     -- will signal an error, because every character must have a name.
-     character.name = ""
+    -- name: The name of the character as a string.  Right now we set
+    -- it as an empty string because the loop later through
+    -- 'properties' should give it a value.  If it does not then we
+    -- will signal an error, because every character must have a name.
+    character.name = ""
 
-     -- color: The color that we use for lines this character speaks
-     -- during a scene.  We expect this to be a table of three integers
-     -- representing the red, green, and blue values of the colors,
-     -- with values in the 0--255 range.
-     character.color = {0, 0, 0}
+    -- color: The color that we use for lines this character speaks
+    -- during a scene.  We expect this to be a table of three integers
+    -- representing the red, green, and blue values of the colors,
+    -- with values in the 0--255 range.
+    character.color = {0, 0, 0}
 
-     -- Overwrite any default property values above with ones given to
-     -- the constructor.
-     for name,value in pairs(properties) do
-         if rawget(character, name) ~= nil then
-             rawset(character, name, value)
-         end
-     end
+    -- Overwrite any default property values above with ones given to
+    -- the constructor.
+    for name,value in pairs(properties) do
+        if rawget(character, name) ~= nil then
+            rawset(character, name, value)
+        end
+    end
 
-     -- Make sure the character has a name, because we do not support
-     -- unnamed characters.
-     if character.name == nil or character.name == "" then
-         error("Cannot create unnamed character")
-     end
+    -- Make sure the character has a name, because we do not support
+    -- unnamed characters.
+    if character.name == nil or character.name == "" then
+        error("Cannot create unnamed character")
+    end
 
-     return character
- end
+    return character
+end
 
 -- This is the method that characters use to speak in scripts.  It
 -- accepts a string of text as an argument and returns a 'say' opcode
