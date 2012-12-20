a25bbc4107bdbc2d9626092ba160134fcf993a81
Eric James Michael Ritz <lobbyjones@gmail.com>
2012-12-19 21:08:04 -0500

    Create a bare-bones LNVL.Graphics module
    This is where we will factor out general-purpose drawing and rendering
    code for the rest of the engine to use.

---
+++ src/graphics.lua
@@ -0,0 +1,14 @@
+--[[
+--
+-- This module provides a namespace of methods for drawing things: the
+-- LNVL.Graphics class.  All methods are static and there is no reason
+-- to create instances of the class.
+--
+--]]
+
+-- Create the LNVL.Graphics class.
+LNVL.Graphics = {}
+LNVL.Graphics.__index = LNVL.Graphics
+
+-- Return the class as the module.
+return LNVL.Graphics
