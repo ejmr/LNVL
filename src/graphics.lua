73244d325a5058f77980cf7224cd54a293cf08b1
Eric James Michael Ritz <lobbyjones@gmail.com>
2012-12-19 22:24:34 -0500

    Factor out the drawText() code into LNVL.Graphics
    This will make it possible to later support things such as appending
    text if we so desire, but for now LNVL.Scene:drawText() maintains its
    behavior of clearing the container before it renders each line of
    given dialog.

--- src/graphics.lua
+++ src/graphics.lua
@@ -44,5 +44,45 @@ function LNVL.Graphics.drawContainer(arguments)
                             LNVL.Settings.Scenes.Height)
 end
 
+-- This function draws text to the screen.  The first argument must be
+-- a Font object from LÖVE.  The second argument can be either a
+-- string or a table.  If it is a string then we render the text using
+-- the current foreground color, which must be set before calling this
+-- function.  If it is a table then we iterate through its contents
+-- like an array; if the element is a string then we print that text,
+-- and if it is a table then we set the foreground color to that,
+-- under the assumption the table has three numeric values, i.e. RGB
+-- values.  Users should use drawContainer() above to clear it before
+-- drawing text.  The function returns no value.
+function LNVL.Graphics.drawText(font, text)
+    love.graphics.setFont(font)
+
+    -- We use this function to iterate through the 'text' argument if
+    -- it happens to be a table, handling the string and table
+    -- elements appropriately.
+    local process =
+        function(element)
+            if type(element) == "string" then
+                love.graphics.printf(element,
+                                     LNVL.Settings.Scenes.X + 10,
+                                     LNVL.Settings.Scenes.Y + 10,
+                                     LNVL.Settings.Scenes.Width - 10,
+                                     "left")
+            elseif type(element) == "table" then
+                love.graphics.setColor(element)
+            end
+        end
+
+    -- If the 'text' argument is just a string then make a simple
+    -- array with the Scene foreground color as the first element and
+    -- the string as the second.  That way we can assume 'text' is
+    -- always an array and process it using one loop below.
+    if type(text) == "string" then
+        text = { self.foregroundColor, text }
+    end
+
+    for _,element in ipairs(text) do process(element) end
+end
+
 -- Return the class as the module.
 return LNVL.Graphics
