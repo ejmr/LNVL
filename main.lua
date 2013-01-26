cc86a9b983acb9978933abe3a43ae560ab3184ff
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-01-26 12:04:38 -0500

    Remove outdated information from the file-header in the main example

--- main.lua
+++ main.lua
@@ -4,10 +4,8 @@
 -- LNVL except debugging, since we ship out LNVL as a library for
 -- other programs to use and not really an executable on its own.
 --
--- The program tests if we can create and draw an LNVL.Scene object
--- with a little dialog.  It reads a script filename from the
--- command-line and loads that.  The enter and backspace keys move
--- back and forth through the text, respectively.
+-- It reads a script filename from the command-line and loads that.
+-- The enter and backspace keys move back and forth through the text.
 --
 --]]
 
