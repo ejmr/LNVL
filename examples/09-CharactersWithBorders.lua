af446cabef1406d0b2e3af0ea45bc519b57cd922
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-01-27 09:25:36 -0500

    Add an example script for testing character image borders
    We will use this script for testing border colors and any other
    properties we define for character image borders in the future.

---
+++ examples/09-CharactersWithBorders.lua
@@ -0,0 +1,19 @@
+-- This script tests the use of borders around character images.  And
+-- it muses about the possible greatness of LNVL and my own amazing,
+-- sordid ego.
+
+Eric = LNVL.Character:new{
+    name = "Eric",
+    textColor = LNVL.Color.SlateBlue,
+    borderColor = LNVL.Color.NavyBlue,
+    image = "examples/images/Eric-Normal.png",
+}
+
+START = LNVL.Scene:new{
+    Eric "I wonder if someone will use this engine for any eroge.",
+    Eric "I would take that as a compliment.",
+    Eric "\"I don't see your code powering erotic adult visual novels!\"",
+    Eric "Yes, huge bragging potential there.",
+}
+
+START:setBackground "examples/images/Celestial-Background.jpg"
