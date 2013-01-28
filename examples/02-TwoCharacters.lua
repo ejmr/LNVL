43df637e8abcdd17f2973cc895c318e52bab937f
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-01-27 20:03:04 -0500

    Give examples of defining other scene colors in the constructor

--- examples/02-TwoCharacters.lua
+++ examples/02-TwoCharacters.lua
@@ -8,6 +8,8 @@ Eric = LNVL.Character:new{name = "Eric", textColor = "#c8ffc8"}
 Jeff = LNVL.Character:new{name = "Jeff", textColor = "#c8c8ff"}
 
 START = LNVL.Scene:new{
+    backgroundColor = LNVL.Color.DeepSkyBlue4,
+    borderColor = LNVL.Color.White,
     Jeff "Why did you use a copyrighted song for an example?",
     Eric "Oh come on, who is going to sue us?",
     Jeff "Susumu Hirawhat's-his-name?  You know, the author?",
