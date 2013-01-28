22204cff28f834218cde8f332b5a8fe068464026
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-01-28 10:55:39 -0500

    Add a name to a scene in one example script for testing
    This will let us see that the tostring() output for LNVL.Scene objects
    is what we expect: the name of the scene if given.  We will see that
    output automatically if we enable debugging mode when running this
    example script.

--- examples/10-ChangeSceneBackground.lua
+++ examples/10-ChangeSceneBackground.lua
@@ -2,6 +2,7 @@
 -- images for scenes dynamically as we go through the scene's content.
 
 START = LNVL.Scene:new{
+    name = "Background Changing Test",
     background = "examples/images/Celestial-Background.jpg",
     "I am seriously over-engineering all of this.",
     LNVL.Scene.changeBackgroundTo "examples/images/Sunny-Hill.jpg",
