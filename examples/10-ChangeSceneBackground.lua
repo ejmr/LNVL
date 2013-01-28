86b08710d1a888439373a5a552e978d4c8c209ee
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-01-27 21:32:30 -0500

    Add an example script to test Scene.changeBackgroundTo()
    This script will crash the engine because the resulting ‘set-image’
    instruction cannot change the scene’s background.  This happens
    because the ‘target’ property of the original ‘set-scene-image’ opcode
    does not point to the Scene object to affect.

---
+++ examples/10-ChangeSceneBackground.lua
@@ -0,0 +1,9 @@
+-- This example script makes sure LNVL can change the background
+-- images for scenes dynamically as we go through the scene's content.
+
+START = LNVL.Scene:new{
+    background = "examples/images/Celestial-Background.jpg",
+    "I am seriously over-engineering all of this.",
+    LNVL.Scene.changeBackgroundTo "examples/images/Sunny-Hill.jpg",
+    "Seriously.  I am.",
+}
