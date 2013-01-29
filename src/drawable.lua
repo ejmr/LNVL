04898d2085fcceab2c610d638decddef11f007c2
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-01-29 11:37:43 -0500

    Create a stub for the new Drawable module

---
+++ src/drawable.lua
@@ -0,0 +1,13 @@
+--[[
+--
+-- This class represents drawable objects in LNVL.  These are objects
+-- that we render to the screen, such as character avatars.
+--
+--]]
+
+-- Create the LNVL.Drawable class.
+LNVL.Drawable = {}
+LNVL.Drawable.__index = LNVL.Drawable
+
+-- Return the class as the module.
+return LNVL.Drawable
