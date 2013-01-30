ae4eaa208298682ea5582cd673213e10334b95ad
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-01-30 18:07:43 -0500

    Dynamically define the values for LNVL.Position
    This will make it possible to add more positions in the future and to
    improve debugging by having a list of valid position values.

--- src/position.lua
+++ src/position.lua
@@ -10,16 +10,27 @@
 LNVL.Position = {}
 LNVL.Position.__index = LNVL.Position
 
--- These three positions specify the general area an object should
--- appear but do not give any exact location.  It is up to the object
--- given the position to determine just exactly what it means to be
+-- These positions specify the general area an object should appear
+-- but do not give any exact location.  It is up to the object given
+-- the position to determine just exactly what it means to be
 -- positioned to the 'right', for example.  We assign strings to each
 -- because that improves debugging output, but the values assigned to
 -- these properties are arbitrary; all that truly matters is that they
 -- are distinct from each other.
-LNVL.Position.Center = "Center"
-LNVL.Position.Right = "Right"
-LNVL.Position.Left = "Left"
+--
+-- To make it easier to add positions in the future, i.e. to cut down
+-- on code, we define of all the positions as a list of strings and
+-- then dynamically create the LNVL.Position properties from that.
+
+LNVL.Position.ValidPositions = {
+    "Center",
+    "Right",
+    "Left",
+}
+
+for _,name in ipairs(LNVL.Position.ValidPositions) do
+    LNVL.Position[name] = name
+end
 
 -- Return the table as the module.
 return LNVL.Position
