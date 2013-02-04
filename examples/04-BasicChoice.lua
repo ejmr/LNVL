b1150f1430fc8e39c1cad72ac50fc16b5644a0b0
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-02-04 18:26:01 -0500

    Remove unused methods from the ‘04’ example script
    We do not need to use ‘says’ with characters anymore, and we have
    since renamed jumpTo() to Scene.changeTo().  The logic for menus is
    still not finished though, so we leave those parts as they are.

--- examples/04-BasicChoice.lua
+++ examples/04-BasicChoice.lua
@@ -7,9 +7,9 @@ Jeff = LNVL.Character{name = "Jeff", textColor = "#c8c8ff"}
 Judge = LNVL.Character{name = "The Great Judgini", textColor = "#ffc8c8"}
 
 START = LNVL.Scene{
-    Jeff:says "Isn't this copyright infringement again?",
-    Eric:says "Would you just shut up...",
-    Judge:says "I have reviewed the evidence.  How do you plead?",
+    Jeff "Isn't this copyright infringement again?",
+    Eric "Would you just shut up...",
+    Judge "I have reviewed the evidence.  How do you plead?",
 
     -- Each choice has two parts: a line of text to present to the
     -- player, and the name of the scene to jump to if that is the
@@ -21,27 +21,27 @@ START = LNVL.Scene{
 }
 
 NOT_GUILTY = LNVL.Scene{
-    Eric:says "Not guilty, your Honor.",
-    Judge:says "Denied!  It's opposite day!",
-    Jeff:says "Wait, what the...",
-    LNVL:jumpTo("THE_TRUTH"),
+    Eric "Not guilty, your Honor.",
+    Judge "Denied!  It's opposite day!",
+    Jeff "Wait, what the...",
+    LNVL.Scene.changeTo("THE_TRUTH"),
 }
 
 THE_TRUTH = LNVL.Scene{
-    Eric:says "Ok, so we infringed on copyrighted material...",
-    Jeff:says "You did.",
-    Judge:says "Then I hearby sentence you to hard labor in that prison from Rambo 2.",
-    Jeff:says "What?!?",
-    Eric:says "Oh cool, do we get a visit from Richard Crenna?",
-    Judge:says "No.  He died in 2003, remember?",
-    Eric:says "Oh yeah...",
-    LNVL:jumpTo("END"),
+    Eric "Ok, so we infringed on copyrighted material...",
+    Jeff "You did.",
+    Judge "Then I hearby sentence you to hard labor in that prison from Rambo 2.",
+    Jeff "What?!?",
+    Eric "Oh cool, do we get a visit from Richard Crenna?",
+    Judge "No.  He died in 2003, remember?",
+    Eric "Oh yeah...",
+    LNVL.Scene.changeTo("END"),
 }
 
 END = LNVL.Scene{
-    Jeff:says "I hate you.  I hate you so much.",
-    Eric:says "I love you too buddy.",
-    Jeff:says "So much hate..."
+    Jeff "I hate you.  I hate you so much.",
+    Eric "I love you too buddy.",
+    Jeff "So much hate..."
 }
 
 -- That is how you make arbitrary jumps to other scenes and poor
