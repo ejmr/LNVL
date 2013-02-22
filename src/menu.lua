ec4d7412b5da302bdc44127c3567788643ff0785
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-02-21 22:07:06 -0500

    Create the LNVL.Menu class
    Currently it does nothing but accept a table of properties in the
    constructor, as with most other classes.  Although we define no
    properties or provide any other methods.

---
+++ src/menu.lua
@@ -0,0 +1,23 @@
+--[[
+--
+-- This file implements the LNVL.Menu class.  It provides dialog
+-- scripts with a way of presenting a list of choices to the player,
+-- mapping those choices to different effects such as changing scenes.
+--
+--]]
+
+-- Create the LNVL.Menu class.
+LNVL.Menu = {}
+LNVL.Menu.__index = LNVL.Menu
+
+-- Our constructor for the LNVL.Menu class.  It accepts a table of
+-- properties to set, assigns those to the new Menu object, and
+-- returns it.
+function LNVL.Menu:new(properties)
+    local menu = {}
+    setmetatable(menu, LNVL.Menu)
+    return menu
+end
+
+-- Return the class as the module.
+return LNVL.Menu
