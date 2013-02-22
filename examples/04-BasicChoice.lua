8fdbc70c0126866fe44128bcd550525af130168f
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-02-21 21:43:35 -0500

    Update the constructor calls in the ‘04’ example script
    The script continues to use the old constructors, not the new()
    methods that have been part of LNVL for quite a while now.  This patch
    also changes the calls to the constructor for LNVL.Menu even though
    that class does not yet exist.  We will use new() for its constructor
    when we do write it, for consistency with the rest of the classes, so
    we can go ahead and change the calls in the example.

--- examples/04-BasicChoice.lua
+++ examples/04-BasicChoice.lua
@@ -2,11 +2,11 @@
 -- choice jumps the story to one of two possible scenes.  This example
 -- also demonstrates jumping to other scenes in general.
 
-Eric = LNVL.Character{name = "Eric", textColor = "#c8ffc8"}
-Jeff = LNVL.Character{name = "Jeff", textColor = "#c8c8ff"}
-Judge = LNVL.Character{name = "The Great Judgini", textColor = "#ffc8c8"}
+Eric = LNVL.Character:new{name = "Eric", textColor = "#c8ffc8"}
+Jeff = LNVL.Character:new{name = "Jeff", textColor = "#c8c8ff"}
+Judge = LNVL.Character:new{name = "The Great Judgini", textColor = "#ffc8c8"}
 
-START = LNVL.Scene{
+START = LNVL.Scene:new{
     Jeff "Isn't this copyright infringement again?",
     Eric "Would you just shut up...",
     Judge "I have reviewed the evidence.  How do you plead?",
@@ -14,20 +14,20 @@ START = LNVL.Scene{
     -- Each choice has two parts: a line of text to present to the
     -- player, and the name of the scene to jump to if that is the
     -- selection he makes.
-    LNVL.Menu{
+    LNVL.Menu:new{
         {"Not Guilty", "NOT_GUILTY"},
         {"Obviously Guilty", "THE_TRUTH"}
     },
 }
 
-NOT_GUILTY = LNVL.Scene{
+NOT_GUILTY = LNVL.Scene:new{
     Eric "Not guilty, your Honor.",
     Judge "Denied!  It's opposite day!",
     Jeff "Wait, what the...",
     LNVL.Scene.changeTo("THE_TRUTH"),
 }
 
-THE_TRUTH = LNVL.Scene{
+THE_TRUTH = LNVL.Scene:new{
     Eric "Ok, so we infringed on copyrighted material...",
     Jeff "You did.",
     Judge "Then I hearby sentence you to hard labor in that prison from Rambo 2.",
@@ -38,7 +38,7 @@ THE_TRUTH = LNVL.Scene{
     LNVL.Scene.changeTo("END"),
 }
 
-END = LNVL.Scene{
+END = LNVL.Scene:new{
     Jeff "I hate you.  I hate you so much.",
     Eric "I love you too buddy.",
     Jeff "So much hate..."
