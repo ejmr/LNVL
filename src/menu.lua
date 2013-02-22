97e4fc97d6151de470c6b85c41c824a02eea8ca2
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-02-22 18:48:37 -0500

    Store choices for a menu when calling the LNVL.Menu constructor
    This patch converts the arguments for the constructor into
    LNVL.MenuChoice objects.  This code causes the ‘04’ example script to
    break since that script uses strings for the so-called ‘action’
    parameter of each choice, while the implementation demands that each
    ‘action’ be a function.  Later we will provide logic that turns those
    strings, which name LNVL.Scene objects, into functions that will cause
    the flow of a story to jump to those scenes.

--- src/menu.lua
+++ src/menu.lua
@@ -19,6 +19,40 @@ require("src.choice")
 function LNVL.Menu:new(properties)
     local menu = {}
     setmetatable(menu, LNVL.Menu)
+
+    -- choices: An array of all of the choices available in this menu.
+    -- Each element is an LNVL.MenuChoice object.
+    menu.choices = {}
+
+    -- Overwrite any default property values above with ones given to
+    -- the constructor.
+    for name,value in pairs(properties) do
+        if rawget(menu, name) ~= nil then
+            rawset(menu, name, value)
+        end
+    end
+
+    -- We treat the remaining entries in 'properties' as an array of
+    -- tables, which we turn into choices.  Each table must be an
+    -- array with two elements:
+    --
+    -- 1. The label for the choice as a string.
+    --
+    -- 2. The action to perform for that choice, as a function.  See
+    -- the documentation for the LNVL.MenuChoice:new() method,
+    -- specifically for the 'action' property, for more details about
+    -- this function.
+    --
+    -- We raise an error for any table that does not meet these
+    -- criteria, although the MenuChoice class performs some of this
+    -- error-checking for us.
+    for _,value in pairs(properties) do
+        assert(type(value) == "table" and #value >= 2,
+              "Invalid data for a menu choice.")
+        table.insert(menu.choices,
+                    LNVL.MenuChoice:new{ label=value[1], action=value[2] })
+    end
+
     return menu
 end
 
