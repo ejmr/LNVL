6d6b2284df457c6edb42aa845218b4539ec40979
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-05-01 23:33:46 -0400

    Implement the ‘show-menu’ instruction
    This makes use of the recent menu handler in the ‘show-menu’
    instruction.  It causes no bugs or crashes with example scripts, but
    that said we have no examples which really stress or test this logic
    to find cracks.  So we cannot say that this is a production-ready
    implementation of the instruction.  At best it satisfies the minimum
    we require of engine instructions.

--- src/instruction.lua
+++ src/instruction.lua
@@ -155,6 +155,14 @@ LNVL.Instructions["no-op"] = Instruction:new {
 LNVL.Instructions["show-menu"] = Instruction:new {
     name = "show-menu",
     action = function (arguments)
+        local executed, choice = coroutine.resume(
+            LNVL.Settings.Handlers.Menu,
+            arguments.menu
+        )
+
+        if executed == true then
+            arguments.menu.currentChoiceIndex = choice
+        end
     end
 }
 
