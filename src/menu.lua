7c8cfe2e1e6edb29712d153ea7d9e8070ea29eb5
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-05-01 21:45:22 -0400

    Add a ‘name’ property to Menu objects to help debugging
    Being able to give names to menus will also help make scripts more
    self-documenting, and could pave the way for referential menus in the
    future—not even sure what I mean by that, but hey, who knows.

--- src/menu.lua
+++ src/menu.lua
@@ -20,6 +20,12 @@ function Menu:new(properties)
     local menu = {}
     setmetatable(menu, Menu)
 
+    -- name: A string with the name of the menu.  Currently we use
+    -- this only for debugging purposes.  If not provided by the
+    -- 'properties' argument then we will set this to a default value
+    -- with the memory address of the table representing the menu.
+    menu.name = string.format("Unnamed Menu %s", tostring(menu))
+
     -- choices: An array of all of the choices available in this menu.
     -- Each element is a MenuChoice object.
     menu.choices = {}
