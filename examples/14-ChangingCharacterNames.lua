e1378a144017ad7dc5446a5f2a353e265a785c1f
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-10-29 08:13:34 -0400

    Character: Add a example test script for displayName()

---
+++ examples/14-ChangingCharacterNames.lua
@@ -0,0 +1,34 @@
+-- This example shows how the displayName() method affects the
+-- character name that appears in dialog.
+
+Eric = Character {
+    dialogName = "Ren",
+    firstName = "Eric",
+    lastName = "Ritz",
+    textColor = "#c8ffc8",
+}
+
+Jeff = Character {
+    dialogName = "Wall",
+    firstName = "Jeff",
+    lastName = "Crenshaw",
+    textColor = "#c8c8ff",
+}
+
+START = Scene {
+    name = "Character Display Name Test",
+    boxBackgroundColor = Color.DeepSkyBlue4,
+    borderColor = Color.White,
+
+    Eric "We're back to our old names.",
+    Jeff "Why?",
+    Eric:displayName "firstName",
+    Eric "No reason.",
+    Jeff:displayName "lastName",
+    Jeff "Oh we can change them?  Neat!",
+    Jeff "Wait...",
+    Eric:displayName "fullName",
+    Eric "Who uses only their last name?  Besides Brock Johnson.",
+    Jeff:displayName "default",
+    Jeff "Brock H. 'The H. Stands for Hercules' Johnson, that's who.  Get it right.",
+}
